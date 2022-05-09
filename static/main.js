9cfaeb174b45a507c618c4752e37d5f4f0ca06d0
suemor <3100825062@qq.com>
2022-05-09 17:59:37 +0800

    Feat: Drag Sort

--- static/main.js
+++ static/main.js
@@ -2,7 +2,7 @@
 
 # KStart
 # By: Dreamer-Paul
-# Last Update: 2022.3.27
+# Last Update: 2022.5.9
 
 一个简洁轻巧的起始页
 
@@ -171,6 +171,7 @@ function KStart() {
       obj.window.item[data.window].classList.remove("active");
 
       obj.window.wrap.classList.remove("active");
+      location.reload();
     },
     closeWindow2: () => {
       if(!data.timer){
@@ -406,6 +407,48 @@ function KStart() {
         }
       };
     },
+    // 初始化公共导航列表的拖拽功能
+    initDragNavi: () => {
+      const items = obj.main.sites.childNodes
+      for (item of items){
+        item.addEventListener('dragstart', (event) => {
+          event.dataTransfer.setData("Text",event.target.getAttribute('data-id'))
+        }, null)
+        item.addEventListener('dragover', (event) => {
+          event.preventDefault()
+        }, null)
+
+        item.setAttribute('draggable', true)
+
+        item.addEventListener('drop', (event) => {
+          const fromId= event.dataTransfer.getData('Text')
+          const from = ks.select(`a[data-id='${fromId}']`)
+          const to = event.path.find(num => {
+            if (num.localName === 'a') {
+              return num
+            }
+          })
+          const toId = to.getAttribute('data-id')
+          const sites = data.user_set.sites
+          const _div = ks.create("div");
+
+          from.parentElement.insertBefore(_div,from);
+          from.parentElement.insertBefore(from,to);
+          from.parentElement.insertBefore(to,_div);
+          from.parentElement.removeChild(_div);
+
+
+          const _fromIdValue = sites.indexOf(Number(fromId))
+          const _toIdValue = sites.indexOf(Number(toId))
+          const temp = sites[_fromIdValue]
+          sites[_fromIdValue] = sites[_toIdValue]
+          sites[_toIdValue] = temp
+          methods.setStorage()
+          methods.setMulSelectValue(obj.settings.sites, data.user_set.sites);
+        }, false)
+      }
+    }
+
   };
 
   modifys.initBody();
@@ -453,11 +496,11 @@ function KStart() {
     else{
       console.error("这个一般不会触发吧？");
     }
-
     data.user_set.background && modifys.checkDarkMode();
 
     modifys.changeSearch(data.user_set.search);
     modifys.initSettingForm();
+    modifys.initDragNavi();
   });
 }
 
